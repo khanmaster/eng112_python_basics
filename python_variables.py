96ce08683b0c0f8fd05c72c3d2bd635ddb5a17be
Abdul Haseeb Shahrukh Khan <ahskhan@spartaglobal.com>
2022-07-28 14:35:07 +0100

    python var added

---
+++ python_variables.py
@@ -0,0 +1,49 @@
+
+# testing the env with pycharm using python 3.9
+#print("Shahrukh")
+# print("Shahrukh")
+# print("Shahrukh")
+# print("Shahrukh")
+# print("Shahrukh")
+# print("Shahrukh")
+# print("Shahrukh")
+
+# What are variable
+# var is to help us store data of diff types
+#name = "Maiks 19 + 8" # string
+# age = 99 # int
+# salary = 15.5 # float
+#
+# print(name)
+# print(age)
+# print(salary)
+#
+#
+# # types of variable
+# # int floats 5.5 boolean true or false string
+# print(type(name))
+# print(type(age))
+# print(type(salary))
+
+# take user input with method called input()
+print("please enter your name")
+name = input()
+print("welcome dear")
+print(name)
+
+# create var input() for fist_name, last_name, age, address, salary, course
+# it should start with Hi & welcome message
+# it needs to be printed back to the user
+# print the type of each var
+
+
+
+
+
+
+
+
+
+
+
+
