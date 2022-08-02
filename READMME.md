bd7e5d5a2c0a4c1dd80b968a211260ce768fcefd
Abdul Haseeb Shahrukh Khan <ahskhan@spartaglobal.com>
2022-08-02 09:08:35 +0100

    string & casting lessons added

--- READMME.md
+++ READMME.md
@@ -49,3 +49,70 @@ print(a == b) #false
 print(a != b) #true
 
 print(a * b) # 6
+```
+
+### Builtin methods
+```python
+# functions  methods builtin in python
+greeting = "Hello World!"
+
+# what options are available in python's builtin library
+print(greeting)
+# if we wanted to check if the letters are in string
+print(greeting.isalpha()) # true or false
+# is it in lower case or upper case
+print(greeting.islower()) # Boolean
+print(greeting.isdigit())
+print(greeting.endswith("!"))
+print(greeting.startswith("34"))
+```
+
+#### Strings Concatenation Casting
+- String indexing 
+- `Hello World!`
+- index in python starts with 0
+
+- Hello World!
+  01234567891011
+```python
+# what options are available in python's builtin library
+print(greeting)
+# if we wanted to check if the letters are in string
+print(greeting.isalpha()) # true or false
+# is it in lower case or upper case
+print(greeting.islower()) # Boolean
+print(greeting.isdigit())
+print(greeting.endswith("!"))
+print(greeting.startswith("34"))
+
+greeting = "Hello World!"
+       #    01234567891011
+       #                -1
+print(len(greeting))
+
+print(greeting[-5])
+print(greeting[:5])
+
+
+print only `world` in a print statement using slicing
+print 4th letter from left to right - use correct indexing for all statements
+print 7th index position letter from right to left
+print 6 letter from left to right
+
+example_string = "Jame                                                       "
+print(example_string)
+print(len(example_string))
+# strip()
+print(len(example_string.strip()))
+
+# welcome user with their name and welcome message - name & message must start with capital
+example_text = "here's some text With lot's of text"
+print(example_text.count("text"))
+
+# find a method to bring the statement in capital/small letters then first letter capital
+
+
+# how to replace text within the string
+
+print(example_text.replace("With ", " ,"))
+```
--- operators.py
+++ operators.py
@@ -9,12 +9,100 @@
 # - `<=`
 
 
-a = 4
-b = 2
-print(a > b) # true
-print(a < b) # false
-print(a == b) #false
-print(a != b) #true
-
-print(a * b) # 6
+# a = 4
+# b = 2
+# print(a > b) # true
+# print(a < b) # false
+# print(a == b && >) #false
+# if a == b && b == c do that
+# print(a != b) #true
 #
+# # print(a * b) # 6
+#
+# functions  methods builtin in python
+# greeting = "Hello World!"
+#
+# # what options are available in python's builtin library
+# print(greeting)
+# # if we wanted to check if the letters are in string
+# print(greeting.isalpha()) # true or false
+# # is it in lower case or upper case
+# print(greeting.islower()) # Boolean
+# print(greeting.isdigit())
+# print(greeting.endswith("!"))
+# print(greeting.startswith("34"))
+#
+# greeting = "Hello World!"
+#        #    01234567891011
+#        #                -1
+# print(len(greeting))
+#
+# print(greeting[-5])
+# print(greeting[:5])
+#
+#
+# print only `world` in a print statement using slicing
+# print 4th letter from left to right - use correct indexing for all statements
+# print 7th index position letter from right to left
+# print 6 letter from left to right
+#
+# example_string = "Jame                                                       "
+# print(example_string)
+# print(len(example_string))
+# # strip()
+# print(len(example_string.strip()))
+#
+# # welcome user with their name and welcome message - name & message must start with capital
+# example_text = "here's some text With lot's of text"
+# print(example_text.count("text"))
+#
+# # find a method to bring the statement in capital/small letters then first letter capital
+#
+#
+# # how to replace text within the string
+#
+# print(example_text.replace("With ", " ,"))
+
+#  Concatenation & Casting
+
+first_name = "James"
+mid_name = "Bond"
+last_name = "007"
+age = "47" # int
+address = 33
+account_details  = "2342342324"
+print(str(age))
+
+print(first_name + " " + mid_name + " " + last_name + " " + str(age))
+
+
+
+
+
+# print(first_name)
+# print(mid_name)
+# print(last_name)
+# select ctr /
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
